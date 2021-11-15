edfba3d3c26ceff27124e46b84e8809dcb37b0c0
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-15 09:48:04 +0100

    aggiornamenti automatici

--- database/seeds/TravelsTableSeeder.php
+++ database/seeds/TravelsTableSeeder.php
@@ -17,7 +17,7 @@ class TravelsTableSeeder extends Seeder
             $newTravel = new Travel();
             $newTravel->partenza = $faker->name();
             $newTravel->destinazione = $faker->name();
-            $newTravel->data_partenza = $faker->dateTime();
+            $newTravel->data_partemza = $faker->dateTime();
             $newTravel->data_arrivo = $faker->dateTime();
             $newTravel->prezzo = $faker->randomNumber(3, true);
             $newTravel->image_url =$faker->imageUrl(500, 500, 'animals', true, 'cats' );
